1a90c0528b7fcf60b862cfb49a570774447ec95f
Tom Saury <saurytom@gmail.com>
2024-02-22 14:52:26 +0100

    Menus done v1 (missing statistics)

--- Assets/_Scripts/UI/Menus/HUDBehaviour.cs
+++ Assets/_Scripts/UI/Menus/HUDBehaviour.cs
@@ -2,6 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuBehaviour : BasicPanel<MenuGameState>
+public class HUDBehaviour : BasicPanel<PlayGameState>
 {
 }
