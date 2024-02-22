907670089b264d0dd626694b044c03bde82c7681
Sh6pa <71717569+Sh6pa@users.noreply.github.com>
2024-02-22 21:25:56 +0100

    DeathHandlingForEnemyPooling

--- Assets/_Scripts/Enemy/PoolingEnemy.cs
+++ Assets/_Scripts/Enemy/PoolingEnemy.cs
@@ -6,4 +6,16 @@ public class PoolingEnemy : Poolable
 {
     [SerializeField] private string m_EnemyName = "Enemy";
     public string EnemyName => m_EnemyName;
+
+    private EnemiesPoolManager m_poolManager;
+
+    private void Start()
+    {
+        m_poolManager = EnemiesPoolManager.SharedInstance;
+    }
+
+    public void Recycle()
+    {
+        m_poolManager.UnloadEnemy(this);
+    }
 }
